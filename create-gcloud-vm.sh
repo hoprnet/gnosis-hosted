d12a78341ee42e46c4c110bb98639c30d5d6f98e
Tino Breddin <t@tolbrino.com>
2021-08-15 21:25:45 +0200

    add cloudflare helper functions

--- create-gcloud-vm.sh
+++ create-gcloud-vm.sh
@@ -28,6 +28,12 @@ declare vm_name="${1:-hosted-gnosis-safe}"
 declare vm_name_ip="${vm_name}-ip"
 declare ssh_cfg="${mydir}/gcloud-vm-ssh.cfg"
 
+log "check if file .env exists"
+if [ -f .env ]; then
+  log "sourcing file .env"
+  source .env
+fi
+
 test -f "${ssh_cfg}" && { msg "${ssh_cfg} already exists, delete to force creation of new VM"; exit 1; }
 test -z "${GCLOUD_PROJECT:-}" && { msg "Missing environment variable GCLOUD_PROJECT"; exit 1; }
 
@@ -42,23 +48,51 @@ declare gcloud_bootdisk="--boot-disk-size=10GB --boot-disk-type=pd-standard"
 declare gcloud_image="--image-family=cos-89-lts --image-project=cos-cloud"
 declare gcloud_shielded_vm="--shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring"
 
-log "create static IP address"
+log "check if GCP static IP address exists"
 if ! gcloud compute addresses describe ${vm_name_ip} ${gcloud_project} ${gcloud_region}; then
+  log "create GCP static IP address"
   gcloud compute addresses create ${vm_name_ip} \
     ${gcloud_project} \
     ${gcloud_region}
 fi
 
-log "create new VM"
-gcloud compute instances create ${vm_name} \
-  ${gcloud_project} \
-  ${gcloud_zone} \
-  ${gcloud_machine_type} \
-  ${gcloud_meta} \
-  ${gcloud_tags} \
-  ${gcloud_bootdisk} \
-  ${gcloud_image} \
-  ${gcloud_shielded_vm} \
-  --address=${vm_name_ip}
+declare static_ip=$(gcloud compute addresses describe ${vm_name_ip} ${gcloud_project} ${gcloud_region} | awk '{ print $2; }')
+log "using GCP static IP ${static_ip}"
+
+log "check if GCP VM exists"
+if ! gcloud compute instances describe ${vm_name} ${gcloud_project} ${gcloud_zone}; then
+  log "create GCP VM "
+  gcloud compute instances create ${vm_name} \
+    ${gcloud_project} \
+    ${gcloud_zone} \
+    ${gcloud_machine_type} \
+    ${gcloud_meta} \
+    ${gcloud_tags} \
+    ${gcloud_bootdisk} \
+    ${gcloud_image} \
+    ${gcloud_shielded_vm} \
+    --address=${vm_name_ip}
+fi
+
+declare cf_zone="${CF_ZONE:-}"
+declare cf_email="${CF_EMAIL:-}"
+declare cf_apikey="${CF_APIKEY:-}"
+declare cf_domain="${CF_DOMAIN:-}"
+declare domains="${vm_name}.intnet.${cf_domain} ${vm_name}-transaction-service.intnet.${cf_domain} ${vm_name}-client-gateway.intnet.${cf_domain}"
+
+log "check if Cloudflare credentials are configured"
+if [ -z "${cf_zone}" ] || [ -z "${cf_email}" ] || [ -z "${cf_apikey}" ] || [ -z "${cf_domain}" ]; then
+  log "skipping Cloudflare DNS entry (not configured), requires env vars CF_ZONE, CF_EMAIL, CF_APIKEY, CF_DOMAIN"
+else
+  for domain in ${domains}; do
+    curl -X POST \
+      "https://api.cloudflare.com/client/v4/zones/${cf_zone}/dns_records" \
+      -H "X-Auth-Email: ${cf_email}" \
+      -H "X-Auth-Key: ${cf_apikey}" \
+      -H "Content-Type: application/json" \
+      --data \
+      '{"type":"A","name":"'${domain}'","content":"'${static_ip}'","ttl":120,"priority":10,"proxied":false}'
+  done
+fi
 
 log "finished"
